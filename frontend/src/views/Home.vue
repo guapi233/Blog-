67c8318d46acb00d632da48b9903f61db84178b7
sanSkiller <1290148953@qq.com>
2020-06-08 10:32:51 +0800

    配置less-loader

---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    hello world
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+  @import "../assets/css/base.less";
+
+  div {
+    color: @primaryColor;
+  }
+</style>
