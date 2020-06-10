65afef231c0c08733278826527e41830afded2b1
sanSkiller <1290148953@qq.com>
2020-06-10 09:14:40 +0800

    拆分Nav组件

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,33 +1,6 @@
 <template>
   <div class="container">
-    <nav>
-      <div class="nav-wrap">
-        <div class="nav-item nav-show">
-          <span>主</span>
-          <span>页</span>
-        </div>
-        <div class="nav-item">
-          <span>博</span>
-          <span>客</span>
-        </div>
-        <div class="nav-item">
-          <span>归</span>
-          <span>档</span>
-        </div>
-        <div class="nav-item githide">
-          <span>G</span>
-          <span>I</span>
-          <span>T</span>
-          <span>H</span>
-          <span>U</span>
-          <span>B</span>
-        </div>
-        <div class="nav-item">
-          <span>关</span>
-          <span>于</span>
-        </div>
-      </div>
-    </nav>
+    <Nav />
     <div class="shelter">
       <div class="shelter-wrap">
         <div class="shelter-left">
@@ -54,35 +27,13 @@
 </template>
 
 <script>
-export default {};
+import Nav from "../components/Nav"
+export default {
+  components: {Nav}
+};
 </script>
 
 <style lang="less" scoped>
-nav {
-  width: 100vw;
-  margin-top: 9.61vh;
-  padding: 0 5.34vh;
-
-  .nav-wrap {
-    width: 26.04vw;
-    height: 10.67vh;
-    line-height: 10.67vh;
-    text-align: center;
-    background-color: @titleColor;
-    margin-left: auto;
-    padding-left: 1.56vw;
-    display: flex;
-    justify-content: center;
-
-    .nav-item {
-      margin-right: 1.56vw;
-      font-size: 1.04vw;
-      color: #fff;
-      font-weight: 700;
-    }
-  }
-}
-
 .shelter {
   width: 100vw;
   height: 100vh;
@@ -165,24 +116,6 @@ nav {
 
 // 适配平板
 @media screen and (max-width: 1200px) and (min-width: 768px) {
-  nav {
-    .nav-wrap {
-      width: 35.04vw;
-      height: 10.67vh;
-      justify-content: space-evenly;
-      .nav-item {
-        display: flex;
-        flex-direction: column;
-        font-size: 2.04vw !important;
-        line-height: 1.5;
-        justify-content: center;
-      }
-      .githide {
-        display: none;
-      }
-    }
-  }
-
   .shower {
     width: 80vw !important;
   }
@@ -240,14 +173,6 @@ nav {
 
 // 适配手机
 @media screen and (max-width: 500px) and (min-width: 325px) {
-  .nav-item {
-    display: none;
-  }
-
-  .nav-show {
-    display: block;
-  }
-
   .shower {
     width: 85.65vw !important;
     height: 94vh !important;
@@ -291,14 +216,6 @@ nav {
 
 // 适配iphone5/SE特小机型
 @media screen and (max-width: 325px) {
-  .nav-item {
-    display: none;
-  }
-
-  .nav-show {
-    display: block;
-  }
-
   .shower {
     width: 85.65vw !important;
     height: 94vh !important;
