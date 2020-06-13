46d6eb4a2d12c14b1e24878ef0f0728fe00dc4fc
sanSkiller <1290148953@qq.com>
2020-06-13 16:44:52 +0800

    update:Nav组件添加icon图标

--- frontend/src/components/Nav.vue
+++ frontend/src/components/Nav.vue
@@ -1,21 +1,34 @@
 <template>
   <nav :style="{ marginTop }">
     <div class="nav-wrap" :style="isRight">
+      <!-- 主页 -->
       <router-link to="/" class="nav-item nav-show" tag="div">
         <span>主</span>
         <span>页</span>
+        <i class="iconfont icon-circle active-item"></i>
       </router-link>
+      <!-- 博客页 -->
       <router-link to="/blog" class="nav-item" tag="div">
-        <span>博</span>
-        <span>客</span>
+        <div class="click-listenner" @click="showSearcher">
+          <span>博</span>
+          <span>客</span>
+          <i class="iconfont icon-search active-item search-item"></i>
+        </div>
+        <!-- 检索框 -->
+        <div class="search-wrap" v-if="searcherShow">
+          <input type="text" />
+        </div>
       </router-link>
+      <!-- GitHub -->
       <div @click="toGitHub" class="nav-item">
         <span>产</span>
         <span>出</span>
       </div>
+      <!-- 关于页 -->
       <router-link to="/about" class="nav-item" tag="div">
         <span>关</span>
         <span>于</span>
+        <i class="iconfont icon-circle active-item"></i>
       </router-link>
     </div>
   </nav>
@@ -33,20 +46,34 @@ export default {
       default: true
     }
   },
+  data() {
+    return {
+      searcherShow: false
+    };
+  },
   computed: {
     isRight() {
-      return this.atRight ?{marginLeft: "auto"} : {}
+      return this.atRight ? { marginLeft: "auto" } : {};
     }
   },
   methods: {
     toGitHub() {
-      window.open('https://github.com/guapi233')
+      window.open("https://github.com/guapi233");
+    },
+    showSearcher() {
+      if (this.$router.currentRoute.fullPath === "/blog") {
+        this.searcherShow = !this.searcherShow;
+      }
     }
   }
 };
 </script>
 
 <style lang="less" scoped>
+.router-link-exact-active i {
+  display: inline !important;
+}
+
 nav {
   width: 100%;
   padding: 0 5.34vh;
@@ -67,6 +94,32 @@ nav {
       color: #fff;
       font-weight: 700;
       cursor: pointer;
+      position: relative;
+      z-index: 0;
+
+      .active-item {
+        color: #27ae60;
+        position: absolute;
+        margin-top: -10px;
+        margin-left: -10px;
+        transform: rotate(165deg);
+        z-index: -1;
+        display: none;
+      }
+
+      .search-item {
+        transform: rotate(0deg);
+        font-size: 18px;
+      }
+
+      .search-wrap {
+        position: absolute;
+      }
+
+      .click-listener {
+        display: inline-block;
+        width: 100%;
+      }
     }
   }
 }
