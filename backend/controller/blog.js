b67595f931c656ee3fc0d0953396f0e29c829fcc
sanSkiller <1290148953@qq.com>
2020-06-17 19:23:29 +0800

    update:图片上传以及新建文章

--- backend/controller/blog.js
+++ backend/controller/blog.js
@@ -1,58 +1,57 @@
-const { exec } = require("../db/mysql")
-const marked = require("../utils/marked")
+const { exec } = require("../db/mysql");
+const handler = require("../utils/marked");
 
 /**
  * 获取博客列表
  * @param {*} keyword 关键字
  */
 async function getList(keyword) {
-  let sql = "select * from blog where 1=1 "
+  let sql = "select * from blog where 1=1 ";
 
-  keyword && (sql += `and title like '%${keyword}%' `)
+  keyword && (sql += `and title like '%${keyword}%' `);
 
-  sql += "and state=1 "
-  sql += "order by createtime desc;"
+  sql += "and state=1 ";
+  sql += "order by createtime desc;";
 
-  let result = await exec(sql)
+  let result = await exec(sql);
 
-  return result
+  return result;
 }
-
 
 /**
  * 获取博客详情
  * @param {*} id 博客id
  */
-async function getDetail (id) {
-  if (!id) return null
+async function getDetail(id) {
+  if (!id) return null;
 
-  let sql = `select * from blog where id=${id}`
+  let sql = `select * from blog where id=${id}`;
 
-  let result = await exec(sql)
+  let result = await exec(sql);
 
-  return result[0]
+  return result[0];
 }
- 
 
 /**
  * 新建博客
  * @param {*} blogObj 文章相关内容
  */
-async function newBlog(blogObj) {
-  let title = blogObj.title, content = blogObj.content, createtime = blogObj.createtime
-  
-  if (!title || !content || !createtime) return false
-
+async function newBlog(blogObj, createtime) {
   // 解析markdown
-  content = marked(content)
+  blogObj = handler(blogObj);
 
-  let sql = `insert into blog (title, content, createtime) values('${title}', '${content}', ${createtime})`
+  let title = blogObj.title,
+    content = blogObj.content,
+    sketch = blogObj.sketch;
 
-  let result = await exec(sql)
+  if (!title || !sketch || !content || !createtime) return false;
 
-  return result.insertId
+  let sql = `insert into blog (title, sketch, content, createtime) values('${title}', '${sketch}', '${content}', ${createtime})`;
+
+  let result = await exec(sql);
+
+  return result.insertId;
 }
-
 
 /**
  * 更新博客
@@ -61,26 +60,25 @@ async function newBlog(blogObj) {
  * @param {*} title 新标题
  */
 async function updateBlog(id, title, content) {
-  let sql = `update blog set title='${title}', content='${content}' where id=${id}`
+  let sql = `update blog set title='${title}', content='${content}' where id=${id}`;
 
-  let result = await exec(sql)
+  let result = await exec(sql);
 
-  if (result.affectedRows > 0) return true
-  return false
+  if (result.affectedRows > 0) return true;
+  return false;
 }
-
 
 /**
  * 删除博客
  * @param {*} id 需要被删除的博客
  */
 async function delBlog(id) {
-  let sql = `update blog set state=0 where id=${id}`
+  let sql = `update blog set state=0 where id=${id}`;
 
-  let result = await exec(sql)
-  
-  if (result.affectedRows > 0) return true
-  return false
+  let result = await exec(sql);
+
+  if (result.affectedRows > 0) return true;
+  return false;
 }
 
 module.exports = {
@@ -88,5 +86,5 @@ module.exports = {
   getDetail,
   newBlog,
   updateBlog,
-  delBlog
-}
+  delBlog,
+};
--- backend/routes/blog.js
+++ backend/routes/blog.js
@@ -1,59 +1,79 @@
-const router = require("koa-router")()
-const { getList, getDetail, newBlog, updateBlog, delBlog } = require("../controller/blog")
-const { SuccessModel, FailModel } = require("../utils/resModel")
+const router = require("koa-router")();
+const fs = require("fs");
+const {
+  getList,
+  getDetail,
+  newBlog,
+  updateBlog,
+  delBlog,
+} = require("../controller/blog");
+const { SuccessModel, FailModel } = require("../utils/resModel");
 
+router.prefix("/blog");
 
-router.prefix("/blog")
-
+// 获取博客列表
 router.get("/", async (ctx, next) => {
-  let resData = await getList(ctx.request.query.keyword || "")
+  let resData = await getList(ctx.request.query.keyword || "");
 
-  ctx.body = new SuccessModel(resData, "成功获取列表")
-})
+  ctx.body = new SuccessModel(resData, "成功获取列表");
+});
 
+// 博客详情
 router.get("/detail", async (ctx, next) => {
-  let resData = await getDetail(ctx.request.query.id)
+  let resData = await getDetail(ctx.request.query.id);
 
   if (!resData) {
-    ctx.body = new FailModel("查找失败")
-    return
+    ctx.body = new FailModel("查找失败");
+    return;
   }
 
-  ctx.body = new SuccessModel(resData, "找到了")
-})
+  ctx.body = new SuccessModel(resData, "找到了");
+});
 
-
+// 新建
 router.post("/new", async (ctx, next) => {
-  let result = await newBlog(ctx.request.body || {})
+  let data;
+  try {
+    data = fs.readFileSync(ctx.request.files.blog.path);
+  } catch (e) {
+    ctx.body = new FailModel("新建失败");
+    return;
+  }
 
+  let result = await newBlog(data.toString(), ctx.request.body.createtime || Date.now());
   if (!result) {
-    ctx.body = new FailModel("新建失败")
-    return
+    ctx.body = new FailModel("新建失败");
+    return;
   }
+  ctx.body = new SuccessModel(result, "新建成功");
+});
 
-  ctx.body = new SuccessModel(result, "新建成功")
-})
-
+// 修改博客
 router.post("/modify", async (ctx, next) => {
-  let result = await updateBlog(ctx.request.body.id, ctx.request.body.title, ctx.request.body.content)
+  let result = await updateBlog(
+    ctx.request.body.id,
+    ctx.request.body.title,
+    ctx.request.body.content
+  );
 
   if (result) {
-    ctx.body = new SuccessModel("更新成功")
-    return
+    ctx.body = new SuccessModel("更新成功");
+    return;
   }
 
-  ctx.body = new FailModel("更新失败")
-})
+  ctx.body = new FailModel("更新失败");
+});
 
+// 删除博客
 router.post("/del", async (ctx, next) => {
-  let result = await delBlog(ctx.request.body.id)
+  let result = await delBlog(ctx.request.body.id);
 
   if (result) {
-    ctx.body = new SuccessModel("删除成功")
-    return
+    ctx.body = new SuccessModel("删除成功");
+    return;
   }
 
-  ctx.body = new FailModel("删除失败")
-})
+  ctx.body = new FailModel("删除失败");
+});
 
-module.exports = router
+module.exports = router;
