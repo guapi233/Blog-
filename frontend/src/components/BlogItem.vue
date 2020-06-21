68c80bf90cfe83c5aa0d1964831e8cd462842a51
sanSkiller <1290148953@qq.com>
2020-06-21 12:02:16 +0800

    update:渲染后台数据

--- frontend/src/components/BlogItem.vue
+++ frontend/src/components/BlogItem.vue
@@ -1,17 +1,22 @@
 <template>
   <div class="blog-item">
     <div class="hr-top"></div>
-    <router-link to="/detail" tag="p" class="blog-title">林州别伦火今求</router-link>
-    <p class="blog-createtime">2100-09-24</p>
-    <p
-      class="blog-content"
-    >我就不太满意啊，就和他说你别老是扣分啊！他还挺委屈告诉我我段位太高（我这个赛季就打到王者拿了印记，最高不超过15星的段位)打不过。问我能不能输几把排位掉星耀。这样就不会扣分了。我一想反正印记拿了，段位不段位无所谓再加一心软就同意了。毕竟人家免费帮我肝了80级战令，肝到了就不让他玩了不太厚道况且我也不太玩这个游戏了（崩坏3真香）于是就同意了。结果好家伙，他一晚上，直接给我从王者低星五排安排到星耀5。</p>
-    <router-link to="/detail" class="blog-detail">阅读全文</router-link>
+    <router-link to="/detail" tag="p" class="blog-title">{{ blogItem.otitle.trim() }}</router-link>
+    <p class="blog-createtime">{{ blogItem.createtime }}</p>
+    <p class="blog-content">{{ blogItem.osketch.trim() }}</p>
+    <router-link :to="{name: 'Detail', params: {blog: blogItem}}"  class="blog-detail">阅读全文</router-link>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    blogItem: {
+      type: Object,
+      default: {}
+    }
+  }
+};
 </script>
 
 <style lang="less" scoped>
@@ -27,7 +32,7 @@ export default {};
   .blog-title {
     margin-top: 2.13vh;
     color: @titleColor;
-    font-size: 1.05vw;
+    font-size: 18px;
     font-weight: 700;
     cursor: pointer;
   }
@@ -45,8 +50,8 @@ export default {};
     color: @textColor;
     margin-bottom: 1.07vh;
     line-height: 1.5;
-    font-size: 0.83vw;
-    height: 9.82vh;
+    font-size: 16px;
+    min-height: 9.82vh;
   }
 
   .blog-detail {
